81b3b5fd04b1882ffd99ea371ee2f34c465c7ded
Kiss Bendegúz <kissbendeguz12@gmail.com>
2026-01-29 20:45:46 +0100

    chore: initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./public/**/*.php",
+        "./app/**/*.php",
+        "./app/Views/**/*.php",
+        "./src/**/*.js",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
